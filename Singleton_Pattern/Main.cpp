5492d7c82b40e322698100787f5623298fba6d74
TobeyChao <TobeyChao@qq.com>
2020-10-04 16:17:17 +0800

    TSingleton

--- Singleton_Pattern/Main.cpp
+++ Singleton_Pattern/Main.cpp
@@ -1,9 +1,25 @@
 #include "Singleton.h"
+#include "TSingleton.h"
+#include <iostream>
+class MyClass : public TSingleton<MyClass>
+{
+public:
+	void ShowDesc()
+	{
+		std::cout << "创建单例成功";
+	}
+};
+
+
 int main(int argc, char* argv[])
 {
 	Singleton* instance = Singleton::GetInstance();
 	instance->Print();
 
 	SingletonStarve* instance2 = SingletonStarve::GetInstance();
+
+	MyClass* myClass = new MyClass();
+	//MyClass* myClass2 = new MyClass(); //报错
+	MyClass::GetInstance().ShowDesc();
 	return 0;
 }
---
+++ Singleton_Pattern/TSingleton.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <cassert>
+template <typename T>
+class TSingleton
+{
+public:
+	TSingleton()
+	{
+		assert(!mSingleton);
+		mSingleton = static_cast<T*>(this);
+	}
+
+	virtual ~TSingleton()
+	{
+		assert(mSingleton);
+		mSingleton = nullptr;
+	}
+
+	static T& GetInstance()
+	{
+		assert(mSingleton);
+		return (*mSingleton);
+	}
+
+	static T* GetInstancePtr()
+	{
+		return mSingleton;
+	}
+private:
+	TSingleton(const TSingleton<T>&) = delete;
+	TSingleton& operator=(const TSingleton<T>&) = delete;
+
+protected:
+	static T* mSingleton;
+};
+
+template<typename T> T* TSingleton<T>::mSingleton = nullptr;
